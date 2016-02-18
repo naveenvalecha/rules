b058a30c906422519e86452ef0e61d857c4a66b1
Naveen Valecha <er.naveenvalecha@gmail.com>
2016-02-18 15:22:21 +0530

    Take care of comments #2409059-27

--- tests/src/Integration/Action/SystemMailToUsersOfRoleTest.php
+++ tests/src/Integration/Action/SystemMailToUsersOfRoleTest.php
@@ -128,7 +128,6 @@ class SystemMailToUsersOfRoleTest extends RulesEntityIntegrationTestBase {
       ],
     ];
     $this->container->set('config.factory', $this->getConfigFactoryStub($config));
-    $this->container->set('entity_type.manager', $this->entityTypeManager->reveal());
 
     $this->action = $this->actionManager->createInstance('rules_mail_to_users_of_role');
   }
@@ -218,7 +217,6 @@ class SystemMailToUsersOfRoleTest extends RulesEntityIntegrationTestBase {
       $role_mock = $this->prophesize(RoleInterface::class);
       $role_mock->id()
         ->willReturn($role_name);
-      $roles[] = $role_mock->reveal();
     }
     // Test sending one or zero email to one or two roles.
     return [
